f3964424846f4688e2bc21509c23a5757433601f
Bikin Maharjan <bikinmaharjan10@gmail.com>
2020-04-18 20:22:39 +1000

    Installed dotenv

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
 
-var app = express();
+//Load config files
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
