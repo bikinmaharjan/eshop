d7824a99551b96e53d0868a928509b960e132e81
Bikin Maharjan <bikinmaharjan10@gmail.com>
2020-05-12 23:03:36 +1000

    Prevent XSS attacks using xss-clean package

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 
 //Load config files
 dotenv.config({ path: './config/config.env' });
@@ -46,6 +47,9 @@ app.use(mongoSanitize());
 
 //Set security headers
 app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
