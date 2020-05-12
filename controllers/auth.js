41e955ab9d775a161f1dd690cf531e1678ea83ff
Bikin Maharjan <bikinmaharjan10@gmail.com>
2020-05-12 22:41:53 +1000

    Logout route added to logout the user and clear the cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 // @desc    Get current logged in user
-// @route   POST /api/v1/auth/me
+// @route   Get /api/v1/auth/me
 // @access  Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
@@ -57,6 +57,20 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: user,
+  });
+});
+
+// @desc    Log user out / clear cookies
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
   });
 });
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,10 +12,14 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    //Set Token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
+
+  //Commented to only use Bearer token in header for auth
   // else if (req.cookies.token) {
-  //   token = req.cookies.token
+  //   //Set TOken from cookie
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
